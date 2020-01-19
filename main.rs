15db96b1cce8875a7d1884c33814375f054ef070
mnabi18 <60067057+mnabi18@users.noreply.github.com>
2020-01-19 20:32:01 +0500

    Assignment 7 Question 3

--- main.rs
+++ main.rs
@@ -1,20 +1,18 @@
-mod lib;
 use std::io;
+use libpakg;
 fn main() {
- loop {
-     println!("Please Enter a Number: ");
-     
-     let mut input1 = String::new();
-     io::stdin().read_line(&mut input1)
-     .expect("Failed to read line");
-     let input1: u32 = match input1.trim().parse(){
-         Ok(num) => num,
-         Err(_)=> continue,
-     };
-     println!("You entered : {}", input1);
-     
-     lib::main_mod::sub_mod::table(input1);
-     
-     break;
- }
+    loop {
+    println!("Please Enter a Number: ");
+    
+    let mut input1 = String::new();
+    io::stdin().read_line(&mut input1)
+    .expect("Failed to read line");
+    let input1: u32 = match input1.trim().parse(){
+        Ok(num) => num,
+        Err(_)=> continue,
+    };
+    println!("You entered : {}", input1);
+    libpakg::main_mod::sub_mod::math1(input1);
+    break;
+}
 }
